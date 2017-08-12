4e684849b14ba046845421924248ee7e1ed01136
derRado <touch@derrado.de>
2017-08-12 11:58:51 +0200

    Updated feedreader, made the menu-hidden test more strict  after review

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,7 +26,7 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-        /* TODO: Write a test that loops through each feed
+        /* Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -38,7 +38,7 @@ $(function() {
             });
         });
 
-        /* TODO: Write a test that loops through each feed
+        /* Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -51,9 +51,9 @@ $(function() {
         });
     });
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* Write a new test suite named "The menu" */
     describe('The menu', function() {
-        /* TODO: Write a test that ensures the menu element is
+        /* Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
@@ -63,28 +63,25 @@ $(function() {
             expect($("body").hasClass('menu-hidden')).toBe(true);
         });
 
-         /* TODO: Write a test that ensures the menu changes
+         /* Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
         it('changes visibility when menu icon is clicked', function() {
-            // Set the starting state of the body tag class
-            let startingState = $("body").hasClass('menu-hidden');
-
-            // Toggle menu visibility and expect the state to be different than the starting state
+            // First click on the menu should remove the menu-hidden class from the body
             $('.menu-icon-link').click();
-            expect($("body").hasClass('menu-hidden')).not.toBe(startingState);
+            expect($("body").hasClass('menu-hidden')).toBe(false);
 
-            // Toggle menu visibility again and expect the state to be equal to the starting state again
+            // Second click on the menu should add the menu-hidden class to the body
             $('.menu-icon-link').click();
-            expect($("body").hasClass('menu-hidden')).toBe(startingState);
+            expect($("body").hasClass('menu-hidden')).toBe(true);
         });
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function() {
-        /* TODO: Write a test that ensures when the loadFeed
+        /* Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
@@ -105,9 +102,9 @@ $(function() {
         });
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+    /* Write a new test suite named "New Feed Selection" */
     describe('New Feed Selection', function() {
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /* Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
@@ -116,11 +113,6 @@ $(function() {
         beforeEach(function(done) {
             // Fire off the loadFeed function and pass the done function as a callback so it gets fired afterwards
             loadFeed(0, done);
-        });
-
-        // Reset the feed
-        afterEach(function() {
-            loadFeed(0);
         });
 
         // This test relies on another async operation, so we will pass done into it
